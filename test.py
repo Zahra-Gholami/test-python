701638a11c958e3c0f07897dbd8683cb5851888b
Zahra Gholami Mendi <zahra.gholami.mend@gmail.com>
2024-02-21 15:28:23 +0330

    develope project

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 username=input("enter your name: ")
 family = input("enter your family: ")
-print("dear %s %s \n wellcome to my app and i'm so happy that you use my project." % (username, family))
+if username == "zahra"  and family =="gholami" :
+    print("dear %s %s \n wellcome to my app and i'm so happy that you use my project." % (username, family))
+else:
+    print("invalid name")
